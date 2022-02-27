b3cd4aec608fc1d4c1d23b532d1eb414342f5b51
FunGuardian <seski.ramadhan@gmail.com>
2022-02-27 23:22:45 +0700

    installing knative on kubernetes

---
+++ install_knative.sh
@@ -0,0 +1,9 @@
+# Install Knative
+kubectl apply -f https://github.com/knative/operator/releases/download/knative-v1.2.0/operator.yaml
+kubectl config set-context --current --namespace=default
+kubectl apply -f knative-serving.yaml
+kubectl apply -f kourier.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.2.0/serving-default-domain.yaml
+# Testing
+kubectl get KnativeServing knative-serving -n knative-serving
+
